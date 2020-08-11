9d98d538fb5d8958ecf947bfe23f15baa99e9c58
sabidebattista <sabidebattista23@gmail.com>
2020-08-11 20:35:02 -0300

    button

--- js/login.js
+++ js/login.js
@@ -3,12 +3,14 @@ function validarForm(){
     let contra = document.getElementById("contra").value[0];
     if ((usuario == null) || (contra == null)) { 
     alert("Los campos no pueden quedar vacios");
-    return false;
-    } else{ return true};
+    } else{ 
+        window.location.href = "principal.html";
+    };
 }
+
 //Función que se ejecuta una vez que se haya lanzado el evento de
 //que el documento se encuentra cargado, es decir, se encuentran todos los
 //elementos HTML presentes.
 document.addEventListener("DOMContentLoaded", function(e){
-    document.getElementById("formulario").onsubmit = "return validarForm()";
+
 });
