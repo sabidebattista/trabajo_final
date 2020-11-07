2e207c12b752b5e58a1d8f7057ef987ba2c773c5
sabidebattista <sabidebattista23@gmail.com>
2020-11-07 15:37:06 -0300

    Proflie2

--- js/my-profile.js
+++ js/my-profile.js
@@ -1,12 +1,86 @@
 /*,apellido,email,edad,direcc,cel*/
-let Pnombre = document.getElementById('Pnombre').value[0];
+/* let Pnombre = document.getElementById('Pnombre').value[0];
 
 function guardarDatos(dato){
     
     datex = localStorage.setItem("Pnombre",JSON.stringify(dato));
     alert(datex);
    
+} */
+
+
+
+/* function mostrarDatos(){
+
+nombre = document.getElementById('Pnombre').value;
+apellido = document.getElementById('Papellido').value;
+email = document.getElementById('Pemail').value;
+edad = document.getElementById('Pedad').value;
+direcc = document.getElementById('Pdirecc').value;
+cel = document.getElementById('Pcel').value;
+
+
+    htmlToAppend = '';
+    htmlToAppend += `
+    <div>
+    <p> Nombre: ${nombre} </p>
+    <p> Apellido: ${apellido} </p>
+    <p> Email: ${email} </p>
+    <p> Edad: ${edad} </p>
+    <p> Dirección: ${direcc} </p>
+    <p> Celular: ${cel} </p>
+    </div>
+    `
+    document.getElementById('mostrarDatos').innerHTML = htmlToAppend;
+
+} */
+
+nombre = document.getElementById('Pnombre');
+apellido = document.getElementById('Papellido');
+email = document.getElementById('Pemail');
+edad = document.getElementById('Pedad');
+direcc = document.getElementById('Pdirecc');
+cel = document.getElementById('Pcel');
+
+function guardarDatos(nomb,ape,email,edad,direcc,cel){
+
+localStorage.setItem('nomb',nomb);
+localStorage.setItem('ape',ape.trim());
+localStorage.setItem('email',email.trim());
+localStorage.setItem('edad',edad.trim());
+localStorage.setItem('direcc',direcc.trim());
+localStorage.setItem('cel',cel.trim());
+
 }
+
+
+function muestro(){
+    var nombre = localStorage.getItem("nomb");
+    var apellido = localStorage.getItem("ape");
+    var email = localStorage.getItem("email");
+    var edad = localStorage.getItem("edad");
+    var direcc = localStorage.getItem("direcc");
+    var cel = localStorage.getItem("cel");
+    
+    document.getElementById('Pnombre').value = nombre;
+
+    htmlToAppend = '';
+    htmlToAppend += `
+    <div>
+    <p> Nombre: ${nombre} </p>
+    <p> Apellido: ${apellido} </p>
+    <p> Email: ${email} </p>
+    <p> Edad: ${edad} </p>
+    <p> Dirección: ${direcc} </p>
+    <p> Celular: ${cel} </p>
+    </div>
+    `
+
+    document.getElementById('mostrarDatos').innerHTML = htmlToAppend;
+
+}
+
+
 
 
 
