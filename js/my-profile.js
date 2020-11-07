63837f25e3d160d2123c7ee341bc6c97275863aa
sabidebattista <sabidebattista23@gmail.com>
2020-11-07 13:55:32 -0300

    Profile1

--- js/my-profile.js
+++ js/my-profile.js
@@ -1,6 +1,20 @@
+/*,apellido,email,edad,direcc,cel*/
+let Pnombre = document.getElementById('Pnombre').value[0];
+
+function guardarDatos(dato){
+    
+    datex = localStorage.setItem("Pnombre",JSON.stringify(dato));
+    alert(datex);
+   
+}
+
+
+
 //Función que se ejecuta una vez que se haya lanzado el evento de
 //que el documento se encuentra cargado, es decir, se encuentran todos los
 //elementos HTML presentes.
 document.addEventListener("DOMContentLoaded", function (e) {
 
+    
+    
 });
